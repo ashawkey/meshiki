6c75d25c388f3be7e5f472b4f36d26cafe0e0170
ashawkey <ashawkey1999@gmail.com>
2025-02-12 01:00:53 -0800

    fix publish

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description="Unusual mesh processing tools",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/ashawkey/meshiki"
+    url="https://github.com/ashawkey/meshiki",
     author="kiui",
     packages=find_packages(),
     include_package_data=True,
